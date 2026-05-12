59af8d22ff86e5e39a4fad5c3d784fd2628b21b9
Learrpa <joyaprilyntacugue30@gmail.com>
2026-05-12 15:21:50 +0800

    update code

--- my-react-app/src/Pages/About.tsx
+++ my-react-app/src/Pages/About.tsx
@@ -3,9 +3,10 @@
 export default function About() {
   return (
     <div className="bg-black min-w-95 min-h-10 text-white">
-        <h1 className="text-3xl font-bold flex justify-center font-anonymous text-2xl">About Me</h1>
+        <h1 className="text-yellow-500 text-3xl font-bold flex justify-center font-anonymous text-2xl p-10">About Me</h1>
         <div className="bg-black">
 
+        
           
 
       </div>
